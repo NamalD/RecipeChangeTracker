b7b7ed0507bde7f56b98e626b2140c801f14599d
Namal Dayarathna <dayarathnand@gmail.com>
2020-07-14 13:07:15 +0100

    Remove Option from RecipeStore.T.Recipes

--- RecipeChangeTracker.Tests/RecipeStore.fs
+++ RecipeChangeTracker.Tests/RecipeStore.fs
@@ -3,38 +3,46 @@ module RecipeChangeTracker.Tests.RecipeStoreTests
 open NUnit.Framework
 open RecipeChangeTracker
 
-let ingredients = [ Ingredient.Unitless { Quantity = Ingredient.Quantity.Integer(1); Name = "pineapple" } ]
+let ingredients =
+    [ Ingredient.Unitless
+        { Quantity = Ingredient.Quantity.Integer(1)
+          Name = "pineapple" } ]
+
 let steps = [ "Peel"; "Eat" ]
 let timeToCook = System.TimeSpan.FromMinutes 20.0
-let testRecipe = Recipe.create "Test Recipe" ingredients steps timeToCook
+
+let testRecipe =
+    Recipe.create "Test Recipe" ingredients steps timeToCook
 
 [<TestFixture>]
-type AddRecipe () =
+type AddRecipe() =
 
     [<Test>]
     member this.``Add Recipe Updates Stored Recipes``() =
-        let store = RecipeStore.create None
+        let store = RecipeStore.create []
 
         let expectedRecipeTree = Some testRecipe
 
         let updatedRecipeStore = RecipeStore.addRecipe testRecipe store
-        let latestRecipe = RecipeList.latest updatedRecipeStore.Recipes.Value.Head
 
-        Assert.That(latestRecipe, Is.EqualTo(expectedRecipeTree));
+        let latestRecipe =
+            RecipeList.latest updatedRecipeStore.Recipes.Head
+
+        Assert.That(latestRecipe, Is.EqualTo(expectedRecipeTree))
 
 [<TestFixture>]
-type DeleteRecipe () =
-    
+type DeleteRecipe() =
+
     [<Test>]
-    member this.``Delete non-existent recipe does nothing when store is empty`` () =
-        let store = RecipeStore.create None
-        
+    member this.``Delete non-existent recipe does nothing when store is empty``() =
+        let store = RecipeStore.create []
+
         let updatedStore = RecipeStore.deleteRecipe store "blah"
 
         Assert.That(updatedStore, Is.EqualTo(store))
 
     [<Test>]
-    member this.``Delete non-existent recipe does nothing when a recipe exists`` () =
+    member this.``Delete non-existent recipe does nothing when a recipe exists``() =
         let store = RecipeStore.createFromRecipe testRecipe
 
         let updatedStore = RecipeStore.deleteRecipe store "foo"
@@ -42,13 +50,19 @@ type DeleteRecipe () =
         Assert.That(updatedStore, Is.EqualTo(store))
 
     [<Test>]
-    member this.``Delete existing recipe removes recipe`` () =
-        let expectedRecipe = { testRecipe with Name = "Another Recipe" }
-        let expectedRecipes = [ Some expectedRecipe; ]
-        let store = 
-            RecipeStore.create None
+    member this.``Delete existing recipe removes recipe``() =
+        let expectedRecipe =
+            { testRecipe with
+                  Name = "Another Recipe" }
+
+        let expectedRecipes = [ Some expectedRecipe ]
+
+        let store =
+            RecipeStore.create []
             |> RecipeStore.addRecipe testRecipe
-            |> RecipeStore.addRecipe { testRecipe with Name = "Another Recipe" }
+            |> RecipeStore.addRecipe
+                { testRecipe with
+                      Name = "Another Recipe" }
 
         let updatedRecipes =
             RecipeStore.deleteRecipe store testRecipe.Name
--- RecipeChangeTracker/Models.fs
+++ RecipeChangeTracker/Models.fs
@@ -2,59 +2,47 @@
 
 open System
 
-// Add recipe
-
-// Delete recipe
-
-// Update recipe
-
-// Get latest recipe
-
-// Get historical recipe
-
-// Undo/redo to state 
 module Ingredient =
-   
+
     type Quantity =
         | Integer of int
         | Float of float
 
-    type BasicIngredient = {
-        Name: string 
-        Quantity: Quantity
-    }
-    
+    type BasicIngredient = { Name: string; Quantity: Quantity }
+
     type T =
-        | WithUnit of Ingredient: BasicIngredient * Quantity: string 
-        | Unitless of Ingredient: BasicIngredient 
+        | WithUnit of Ingredient: BasicIngredient * Quantity: string
+        | Unitless of Ingredient: BasicIngredient
 
 module Recipe =
-    
-    type T = {
-        Name: string
-        Ingredients: Ingredient.T list
-        Steps: string list
-        CookTime: TimeSpan
-    }
+
+    type T =
+        { Name: string
+          Ingredients: Ingredient.T list
+          Steps: string list
+          CookTime: TimeSpan }
 
     let create name ingredients steps cooktime =
-        { Name = name; Ingredients = ingredients; Steps = steps; CookTime = cooktime }
+        { Name = name
+          Ingredients = ingredients
+          Steps = steps
+          CookTime = cooktime }
 
 module RecipeList =
-    
-    type Node = {
-        Recipe: Recipe.T Option
-        Id: Guid
-        PreviousId: Guid Option
-    }
+
+    type Node =
+        { Recipe: Recipe.T Option
+          Id: Guid
+          PreviousId: Guid Option }
 
     type T = Node list
 
     let create recipe =
-        [{ Recipe = recipe; Id = Guid.NewGuid(); PreviousId = None }]
+        [ { Recipe = recipe
+            Id = Guid.NewGuid()
+            PreviousId = None } ]
 
-    let latest (tree:T) =
-        tree.Head.Recipe
+    let latest (tree: T) = tree.Head.Recipe
 
     let getNodeWithId tree id =
         List.tryPick (fun node -> if node.Id = id then Some node else None) tree
@@ -64,19 +52,24 @@ module RecipeList =
         | None -> None
         | Some previousId -> getNodeWithId tree previousId
 
-    let getHeadId (tree:T) = 
+    let getHeadId (tree: T) =
         match tree.Head.Recipe with
         | None -> None
         | Some _ -> Some tree.Head.Id
 
-    let getRecipeName (tree:T) =
+    let getRecipeName (tree: T) =
         match tree.Head.Recipe with
         | None -> None
         | Some recipe -> Some recipe.Name
 
-    let update (tree:T) newRecipe =
+    let update (tree: T) newRecipe =
         let previousId = getHeadId tree
-        let newHead = { Recipe = Some newRecipe; Id = Guid.NewGuid(); PreviousId = previousId }
+
+        let newHead =
+            { Recipe = Some newRecipe
+              Id = Guid.NewGuid()
+              PreviousId = previousId }
+
         newHead :: tree
 
     let matchesName name list =
@@ -85,39 +78,29 @@ module RecipeList =
 
 module RecipeStore =
 
-    type T = {
-        Recipes: RecipeList.T list Option
-    }
+    type T = { Recipes: RecipeList.T list }
 
-    let create recipes =
-        { Recipes = recipes }
+    let create recipes = { Recipes = recipes }
 
     let createFromRecipe recipe =
         let recipeList = RecipeList.create (Some recipe)
-        { Recipes = Some [ recipeList ] }
+        { Recipes = [ recipeList ] }
 
     let addRecipe recipe store =
         let recipeTree = RecipeList.create (Some recipe)
 
-        let updatedRecipes = 
-            match store.Recipes with
-            | None -> Some [ recipeTree ]
-            | Some existingRecipes -> Some (recipeTree :: existingRecipes)
+        let updatedRecipes = recipeTree :: store.Recipes
 
         { store with Recipes = updatedRecipes }
 
-    let getLatestRecipes store =
-        match store.Recipes with
-        | None -> []
-        | Some recipes -> List.map RecipeList.latest recipes
-    
+    let getLatestRecipes store = List.map RecipeList.latest store.Recipes
+
     let excludingRecipe recipeToExclude recipes =
         List.where (RecipeList.matchesName recipeToExclude >> not) recipes
 
     let deleteRecipe store recipeToDelete =
-        let recipesWithoutDeleted = 
-            match store.Recipes with
-            | None -> None
-            | Some recipes -> Some (excludingRecipe recipeToDelete recipes)
+        let recipesWithoutDeleted =
+            excludingRecipe recipeToDelete store.Recipes
 
-        { store with Recipes = recipesWithoutDeleted }
+        { store with
+              Recipes = recipesWithoutDeleted }
